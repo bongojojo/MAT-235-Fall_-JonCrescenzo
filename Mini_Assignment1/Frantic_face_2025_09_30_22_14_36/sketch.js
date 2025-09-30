890b2ee6c4c72ff26dbb60da1c57e0ac95f91551
bongojojo <64935699+bongojojo@users.noreply.github.com>
2025-09-30 15:50:25 -0700

    Add files via upload

---
+++ Mini_Assignment1/Frantic_face_2025_09_30_22_14_36/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rectMode(CENTER);
+  fill(255, 242, 21);
+  noStroke();
+  rect(200, 200, 300, 300);
+    
+  fill(5);
+  circle(154, 150, 100);
+  circle(245, 150, 100);
+  
+  fill(255);
+  circle(154, 150, 20);
+  circle(245, 150, 20);
+    
+  
+  fill(5);
+  ellipse(200, 275, 200, 75);
+    
+
+      
+  
+  fill(255);
+  rect(210, 255, 20, 40);
+    rect(180, 255, 20, 40);
+    
+  
+  
+  fill(1);
+  angleMode(DEGREES);
+  rotate(5);
+  rect(250, 65, 100, 13);
+  
+  rotate(-10);
+    rect(150, 95, 100, 13);
+
+  
+
+  
+
+  
+  
+  
+
+}
